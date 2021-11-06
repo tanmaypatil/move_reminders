9a96c76e820a761331253203f4a91293b70186f7
Tanmay Patil <tany.patil77@gmail.com>
2021-11-06 16:50:44 +0530

    pushing new lambda using github actions

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ exports.handler =  async function(event, context) {
     console.log(` start execution of lambda ${context.functionName}`);
     console.log("EVENT: \n" + JSON.stringify(event, null, 2));
     console.log(`move alarm - alarm date ${event.alarmDate} , alarm id - ${event.alarmId} `);
+    if ( !event.alarmDate || !event.alarmId) {
+      console.log('mandatory argument not provided');
+      return Error('Either alarmDate or alarmId should be provided');
+    }
     move_alarms(event.alarmDate,event.alarmId);
 
     return context.logStreamName
