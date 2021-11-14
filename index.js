b47113c7c2e878d878d1613fe3a3370cf9cff239
Tanmay Patil <tany.patil77@gmail.com>
2021-11-14 18:37:49 +0530

    fixed issue in calling moveAlarm

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const move_alarms = require("./move_alarms");
 
 exports.handler =  async function(event, context) {
+    let alarmObj = {};
     console.log(` start execution of lambda ${context.functionName}`);
     console.log("EVENT: \n" + JSON.stringify(event, null, 2));
     console.log(`move alarm - alarm date ${event.alarmDate} , alarm id - ${event.alarmId} `);
     if ( !event.alarmDate || !event.alarmId) {
       console.log('not present in event object directly');
-      let alarmObj = JSON.parse(event.body);
+      alarmObj = JSON.parse(event.body);
       console.log('move alarm - body after parsing '+JSON.stringify(alarmObj));
       if(!alarmObj.alarmDate || !alarmObj.alarmId) {
       console.log('mandatory argument not provided');
       return Error('Both alarmDate or alarmId should be provided');
       }
     }
-    let data = await move_alarms.moveAlarm(event.alarmDate,event.alarmId);
+    else {
+      alarmObj.alarmId = event.alarmId;
+      alarmObj.alarmDate = event.alarmDate;
+    }
+    console.log("before calling moveAlarm "+JSON.stringify(alarmObj));
+    let data = await move_alarms.moveAlarm(alarmObj.alarmDate,alarmObj.alarmId);
     console.log("before return lambda function "+JSON.stringify(data));
 
     return context.logStreamName
