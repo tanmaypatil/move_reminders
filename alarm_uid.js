27387257cc876089a6555f7dac18cef089f2c5d0
Tanmay Patil <tany.patil77@gmail.com>
2021-12-03 23:55:53 +0530

    corrected defect for reaching actions

--- alarm_uid.js
+++ alarm_uid.js
@@ -5,7 +5,7 @@ function getUidOfAlarm(str) {
     let bare_payload = payload.substr(8);
     let obj = JSON.parse(bare_payload);
     console.log('getUidOfAlarm - after parse '+JSON.stringify(obj));
-    let actions = obj.message.actions;
+    let actions = obj.actions;
     console.log('getUidOfAlarm - actions from payload '+JSON.stringify(actions));
     // Assuming only 1 button clicked 
     let uid = actions[0].value;
